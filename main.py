921e9517e5f521727f083c94f0fdff3a2b6a26bd
Ashish suman <121541957+Ashish-suman-sharma@users.noreply.github.com>
2024-08-14 19:10:10 +0530

    token id changed

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot("6750932689:AAHSK1ri1eBTwFv1Caycp9N7vBaFEj89VK0")
+bot = telebot.TeleBot("6899288816:AAF61IJ-JcAUH1PcWYZ0g89k-F947FMSMRo")
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     markup = types.InlineKeyboardMarkup()
-    web_app = types.WebAppInfo("https://Ashishsuman.me") 
+    web_app = types.WebAppInfo("https://ashishsuman.me/pepelayer") 
     button = types.InlineKeyboardButton("Open Website", web_app=web_app)
     markup.add(button)
     bot.send_message(message.chat.id, "Welcome! Click the button to open the website.", reply_markup=markup)
